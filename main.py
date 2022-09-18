8bbc41c9850c21e9928db9f38d0bf1f748213eb7
Stephen Titcombe <40187876+stitcombe@users.noreply.github.com>
2022-09-18 16:00:34 +0000

    added uvicorn

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
-@app.get(/)
-def read_root():
-  return {"hello": "world"}
 
-@app.get("/items/{item_id")
+@app.get("/")
+def read_root():
+    return {"hello": "world"}
+
+
+@app.get("/items/{item_id}")
 def read_item(item_id: int, q: str):
-  return {"item_id": item_id, "q": q}
+    return {"item_id": item_id, "q": q}
+
+uvicorn.run(app,host="0.0.0.0",port="8080")
