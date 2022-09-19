67a3ee771ba6e40df4ad50ad40864c039c761220
Stephen Titcombe <40187876+stitcombe@users.noreply.github.com>
2022-09-19 01:15:19 +0000

    update campaign class

--- class/campaign.py
+++ class/campaign.py
@@ -1,16 +1,14 @@
-from datetime import date, datetime
+from datetime import datetime
+
 
 class campaign:
-  def __init__(self, name, description, category, startDate, endDate, fundingGoal, currentFunds):
-    self.name= name
-    self.description = description
-    self.category = category
-    self.startDate = datetime.strptime(startDate, '%Y-%d-%m').date()
-    self.endDate = datetime.strptime(endDate, '%Y-%d-%m').date()
-    self.fundingGoal = fundingGoal
-    self.currentFunds = calcFunds
-  
-  def age(self):
-    today = date.today()
-    age = today.year - self.dob.year - ((today.month, today.day) < (self.dob.month, self.dob.day))
-    return age
+
+    def __init__(self, name, description, category, startDate, endDate,
+                 fundingGoal, currentFunds):
+        self.name = name
+        self.description = description
+        self.category = category
+        self.startDate = datetime.strptime(startDate, '%Y-%d-%m').date()
+        self.endDate = datetime.strptime(endDate, '%Y-%d-%m').date()
+        self.fundingGoal = fundingGoal
+        self.currentFunds = 500
