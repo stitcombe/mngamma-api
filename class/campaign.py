b2f23fb3847d2a7e6dd3e3bf8cb9ee4ad192126c
Stephen Titcombe <40187876+stitcombe@users.noreply.github.com>
2022-09-22 02:34:32 +0000

    lint updates

--- class/campaign.py
+++ class/campaign.py
@@ -2,13 +2,13 @@ from datetime import datetime
 
 
 class campaign:
-
-    def __init__(self, name, description, category, startDate, endDate,
-                 fundingGoal, currentFunds):
+    def __init__(
+        self, name, description, category, startDate, endDate, fundingGoal, currentFunds
+    ):
         self.name = name
         self.description = description
         self.category = category
-        self.startDate = datetime.strptime(startDate, '%Y-%d-%m').date()
-        self.endDate = datetime.strptime(endDate, '%Y-%d-%m').date()
+        self.startDate = datetime.strptime(startDate, "%Y-%d-%m").date()
+        self.endDate = datetime.strptime(endDate, "%Y-%d-%m").date()
         self.fundingGoal = fundingGoal
         self.currentFunds = 500
